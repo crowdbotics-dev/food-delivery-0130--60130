9d1f43da89e36d85ab87bb1a64cd2e358ad91d7d
crowdbotics-dev <engineering@crowdbotics.com>
2023-01-30 16:44:17 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class Restaurant(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=200,)
+    address = models.CharField(max_length=200,)
+    phone = models.CharField(max_length=200,)
+    website = models.URLField()
